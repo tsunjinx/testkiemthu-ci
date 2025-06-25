b4a9d8587790c2b57f1698a2afe0b32e9e3033f5
Hoang Manh Duy <42913053+tsunjinx@users.noreply.github.com>
2025-06-26 01:01:40 +0700

    Add default context test

---
+++ src/test/java/com/example/testkiemthu/TestkiemthuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testkiemthu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestkiemthuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
